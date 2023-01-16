b9d39c172dd7257c2377fa2d4717fb3ad6c1a2f7
ShivamKumarSa <shivam.sachdeva@block8.com>
2023-01-16 14:16:04 +0530

    deploying to gcp container

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
 
-        <h1>Welcome To My POC</h1>
+        <h1>Welcome To My POC On Docker</h1>
         <p className="App-link">Learn React With Shivam Sachdeva</p>
       </header>
     </div>
