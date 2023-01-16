10465fb1c78560bdadf103f2ba05e8ec0cc24804
ShivamKumarSa <shivam.sachdeva@block8.com>
2023-01-16 16:15:31 +0530

    small change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
 
-        <h1>Welcome To My POC</h1>
+        <h1>Welcome To My POC2</h1>
         <p className="App-link">
           On Github Actions, Docker And GCP Container Registry
         </p>
